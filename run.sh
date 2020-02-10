9411189a475c214452f6af6d08d014e87448ceaa
Jeroen van den Hoven <jvandenhoven@mobiquityinc.com>
2020-02-10 15:53:56 +0100

    Get basic docker deployment up and running

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-env FLASK_APP=server.app.py flask run
+#!/bin/bash
+
+default_host="127.0.0.1"
+host=${host:-$default_host}
+
+cd server && flask run -h $host
+
+#docker run --name flask-server -p 5000:5000 -it cicdtraining
