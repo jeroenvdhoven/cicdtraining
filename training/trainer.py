158fdde3223ed9b588b3f367b8f4eedb142bffa1
Jeroen van den Hoven <jvandenhoven@mobiquityinc.com>
2020-02-11 11:50:26 +0100

    Add basic training function

---
+++ training/trainer.py
@@ -0,0 +1,60 @@
+import os
+import json
+
+import pandas as pd
+from joblib import dump
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, FunctionTransformer
+
+from .model_functions import call_model, reverse_labels
+
+if __name__ == "__main__":
+    with open("server/model_location_config.json", "r") as f:
+        model_location = json.load(f)
+        output_folder = model_location["output_folder"]
+        model_file = model_location["model_file"]
+
+    # Read data
+    data = pd.read_csv(os.path.join("training", "iris.csv"))
+    data = data[data["Species"].isin(["setosa", "virginica"])]
+
+    # Split into x and y
+    x = data.drop("Species", axis=1)
+    y = data[["Species"]]
+
+    # Make preprocessing pipelines
+    x_pipeline = Pipeline([
+        ("scaling", StandardScaler()),
+    ])
+    x_fit = x_pipeline.fit_transform(x)
+
+    y_pipeline = Pipeline([
+        ("labeler", OrdinalEncoder()),
+    ])
+    y_fit = y_pipeline.fit_transform(y)
+
+    # Fit the pipeline
+    model = LogisticRegression(solver="lbfgs")
+    model.fit(x_fit, y_fit)
+
+    # Create a fully integrated pipeline
+    prediction_pipeline = Pipeline([
+        ("preprocessing", x_pipeline),
+        ("pipeline", FunctionTransformer(func=call_model, kw_args={"model": model})),
+        ("retransform labels", FunctionTransformer(
+            # Turn the 0-1 labels back into setosa / virginica labels
+            func=reverse_labels,
+            validate=False,
+            kw_args={"pipe": y_pipeline}),
+         )
+    ])
+
+    # Try the full pipeline
+    preds = prediction_pipeline.transform(x)
+    print(confusion_matrix(y, preds))
+
+    # Save models
+    os.makedirs(output_folder, exist_ok=True)
+    dump(prediction_pipeline, os.path.join(output_folder, model_file))
