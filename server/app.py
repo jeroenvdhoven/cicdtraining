6077ccc67cc364b1af35fd8bb40f9b51a3e279c0
Jeroen van den Hoven <jvandenhoven@mobiquityinc.com>
2020-02-11 11:16:33 +0100

    Update app.py to check if code gets updated on AWS

--- server/app.py
+++ server/app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return f'Hello, {escape(name)}! This is deployed on travis'
